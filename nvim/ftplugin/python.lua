43fa1b04d2a6ba4b67766acb043bd958a1d2bdb2
Gahan Rakholia <gahan94rakh@gmail.com>
2024-05-12 22:23:49 +0530

    remove run and format overrides from python lua

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -1,13 +1,5 @@
 local lspkeys = require("mykeybindings").lspkeys
 lspkeys = vim.tbl_deep_extend("keep", lspkeys, {
-  ["<SPACE>j"] = {
-    name = "+Format",
-    ["="] = { "<cmd>lua vim.lsp.buf.format({async = true})<CR>", "Format go code" }
-  },
-  ["<SPACE>r"] = {
-    name = "+Run",
-    r = {"<cmd>Dispatch python ".. vim.api.nvim_buf_get_name(0) .."<cr>", "Run current file"}
-  },
   ["<SPACE>l"] = {
     d = {
       T = { "<cmd>lua require('dap-python').test_class()<cr>", "debug current test class" },
